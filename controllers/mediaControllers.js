962679e33f05ed679526e701f38007dfc645fc44
rizqi-r <125800839+rizqi-r@users.noreply.github.com>
2024-05-02 05:15:22 +0700

    feat: imagekit feature and mediaControllers

---
+++ controllers/mediaControllers.js
@@ -0,0 +1,254 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const imagekit = require("../libs/imagekit");
+const path = require("path");
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const getAllPhoto = async (req, res, next) => {
+    try {
+        const jwtUserId = Number(req.user.id);
+
+        if (!jwtUserId) {
+            return res.status(400).json({
+                status: 400,
+                message: "Bad Request"
+            });
+        }
+
+        const photoExist = await prisma.post.findMany({
+            where: {
+                userId: jwtUserId
+            }
+        });
+
+        if (!photoExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "User not found"
+            });
+        }
+
+        photoExist.forEach((i) => {
+            delete i.userId;
+        });
+
+        return res.status(200).json({
+            status: 200,
+            message: "OK",
+            data: photoExist
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const getPhoto = async (req, res, next) => {
+    try {
+        const params = Number(req.params.id);
+        const jwtUserId = Number(req.user.id);
+
+        if (!jwtUserId || !params) {
+            return res.status(400).json({
+                status: 400,
+                message: "Bad Request"
+            });
+        }
+
+        const userExist = await prisma.user.findUnique({
+            where: {
+                id: jwtUserId
+            }
+        });
+
+        if (!userExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "User not found"
+            });
+        }
+
+        const photoExist = await prisma.post.findUnique({
+            where: {
+                userId: jwtUserId,
+                id: params
+            }
+        });
+
+        if (!photoExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "Photo not found"
+            });
+        }
+
+        delete photoExist.userId;
+
+        return res.status(200).json({
+            status: 200,
+            message: "OK",
+            data: photoExist
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const uploadPhoto = async (req, res, next) => {
+    try {
+        const jwtUserId = Number(req.user.id);
+        const { title, description } = req.body;
+        const file = req.file.buffer.toString("base64");
+        console.log(jwtUserId);
+
+        if (!jwtUserId || !file || !title || !description) {
+            return res.status(400).json({
+                status: 400,
+                message: "Bad Request"
+            });
+        }
+
+        const userExist = await prisma.user.findUnique({
+            where: {
+                id: jwtUserId
+            }
+        });
+
+        if (!userExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "User not found"
+            });
+        }
+
+        try {
+            const response = await imagekit.upload({
+                fileName: Date.now() + path.extname(req.file.originalname),
+                file: file
+            });
+
+            const photo = await prisma.post.create({
+                data: {
+                    title,
+                    description,
+                    photoUrl: response.url,
+                    userId: jwtUserId
+                }
+            });
+
+            delete photo.userId;
+
+            return res.status(201).json({
+                status: 201,
+                message: "Created",
+                data: photo
+            });
+        } catch (error) {
+            next(error);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+const updatePhoto = async (req, res, next) => {
+    try {
+        const jwtUserId = Number(req.user.id);
+        const params = Number(req.params.id);
+        const { title, description } = req.body;
+        const file = req.file.buffer.toString("base64");
+
+        if (!jwtUserId) {
+            return res.status(400).json({
+                status: 400,
+                message: "Bad Request"
+            });
+        }
+
+        const userExist = await prisma.user.findUnique({
+            where: {
+                id: jwtUserId
+            }
+        });
+
+        if (!userExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "User not found"
+            });
+        }
+
+        const photoExist = await prisma.post.findUnique({
+            where: {
+                userId: jwtUserId,
+                id: params
+            }
+        });
+
+        if (!photoExist) {
+            return res.status(404).json({
+                status: 404,
+                message: "User or photo not found"
+            });
+        }
+
+        try {
+            let response;
+            if (file) {
+                response = await imagekit.upload({
+                    fileName: Date.now() + path.extname(req.file.originalname),
+                    file: file
+                });
+            }
+
+            const photo = await prisma.post.update({
+                data: {
+                    title: title || photoExist.title,
+                    description: description || photoExist.title,
+                    photoUrl: response.url || photoExist.photoUrl
+                },
+                where: {
+                    userId: jwtUserId,
+                    id: params
+                }
+            });
+
+            delete photo.userId;
+
+            return res.status(201).json({
+                status: 201,
+                message: "Created",
+                data: photo
+            });
+        } catch (error) {
+            next(error);
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    getAllPhoto,
+    getPhoto,
+    uploadPhoto,
+    updatePhoto
+};
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
 const authController = require("../controllers/authControllers");
+const mediaController = require("../controllers/mediaControllers");
+const { image } = require("../libs/mutler");
 const { restrict } = require("../middlewares/middleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/whoami", restrict, authController.whoami);
+
+router.get("/users/photo", restrict, mediaController.getAllPhoto);
+router.get("/users/photo/:id", restrict, mediaController.getPhoto);
+router.post("/users/photo", restrict, image.single("file"), mediaController.uploadPhoto);
+router.put("/users/photo/:id", restrict, image.single("file"), mediaController.updatePhoto);
 
 module.exports = router;
